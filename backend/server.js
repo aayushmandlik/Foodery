2dc1f52173d041ee8cfe2da71e7ef74d66096239
hm-mandu <hritikmandlik63@gmail.com>
2025-03-18 01:02:58 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,14 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://foodery-frontend.onrender.com",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type",
+  })
+);
 
 // db connection
 connectDB();
